02cca23ae536ffbcb32fb6aea65e2dd896b2cb42
Tonoy Akanda <tonoy@monetizenow.io>
2023-11-04 09:28:34 +0600

    ec2 instance

---
+++ datasources.tf
@@ -0,0 +1,10 @@
+data "aws_ami" "frc_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-kernel-6.1-x86_64"]
+  }
+}
+
---
+++ main.tf
@@ -0,0 +1,93 @@
+resource "aws_vpc" "frc_vpc" {
+  cidr_block           = "10.123.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "frc_vpc_dev"
+  }
+}
+
+resource "aws_subnet" "frc_public_subnet" {
+  vpc_id                  = aws_vpc.frc_vpc.id
+  cidr_block              = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-south-1a"
+
+  tags = {
+    Name = "frc_public_subnet_dev"
+  }
+}
+
+resource "aws_internet_gateway" "frc_internet_gateway" {
+  vpc_id = aws_vpc.frc_vpc.id
+
+  tags = {
+    Name = "frc_internet_gateway_dev"
+  }
+}
+
+resource "aws_route_table" "frc_route_table" {
+  vpc_id = aws_vpc.frc_vpc.id
+
+  tags = {
+    Name = "frc_route_table_dev"
+  }
+}
+
+resource "aws_route" "frc_route" {
+  route_table_id         = aws_route_table.frc_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.frc_internet_gateway.id
+}
+
+resource "aws_route_table_association" "frc_route_table_association" {
+  subnet_id      = aws_subnet.frc_public_subnet.id
+  route_table_id = aws_route_table.frc_route_table.id
+}
+
+resource "aws_security_group" "frc_security_group" {
+  name        = "frc_security_group_dev"
+  description = "frc dev security group"
+
+  vpc_id = aws_vpc.frc_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "frc_key_pair" {
+  key_name   = "frcaws_key"
+  public_key = file("/Users/tonoy/.ssh/frcaws.pub")
+}
+
+resource "aws_instance" "frc_ec2_instance" {
+  ami                    = data.aws_ami.frc_ami.id
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.frc_key_pair.id
+  vpc_security_group_ids = [aws_security_group.frc_security_group.id]
+  subnet_id              = aws_subnet.frc_public_subnet.id
+  user_data              = file("./userdata.tpl")
+
+  root_block_device {
+    volume_size = 10
+  }
+
+  tags = {
+    Name = "frc_ec2_instance_dev"
+  }
+}
+
+output "frc_ec2_instance_ssh" {
+  value = "ssh -i ~/.ssh/frcaws ec2-user@${aws_instance.frc_ec2_instance.public_ip}"
+}
